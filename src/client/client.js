0e2b43f64257b79924418c52a310fbd1af5d7a40
Andrew Shaffer <andrew@weo.io>
2016-09-13 17:04:20 -0700

    test

--- src/client/client.js
+++ src/client/client.js
@@ -2,12 +2,10 @@
  * Imports
  */
 
-// import jss from './jss'
+import jss from './jss'
 import 'regenerator-runtime/runtime'
 import promise from 'es6-promise'
 import favicon from 'lib/favicon'
-
-Error.stackTraceLimit = 100
 
 /**
  * Polyfills
